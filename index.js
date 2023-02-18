4928226a3f1003744763be0a779f876a0ca63181
shuisheng <2274751790@qq.com>
2023-02-19 02:19:47 +0800

    注意：当前工作区不干净，已自动保存当前更改

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const chokidar = require('chokidar');
-const { execSync } = require('child_process');
+const { execSync, exec } = require('child_process');
 
 function gitIsClean() {
   const res = execSync('git status -s').toString();
   return res.length > 0 ? false : true;
+}
+
+function gitPush(commitMsg) {
+  try {
+    exec('git push', (error, stdout, stderr) => {
+      if (error || stderr) {
+        console.log(error, stderr);
+        console.log(
+          `${new Date().toLocaleString},提交${commitMsg}到github失败！`
+        );
+        return;
+      }
+      console.log(
+        `${new Date().toLocaleString},提交${commitMsg}到github成功！`
+      );
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 function main() {
@@ -11,15 +30,19 @@ function main() {
     console.log(eventname, path, gitIsClean());
     // git工作区是干净的，就不需要保存
     if (gitIsClean()) return;
+    const commitMsg = eventname + ':' + path;
     execSync(`git add .`);
-    execSync(`git commit -m '${eventname}:${path}'`);
+    execSync(`git commit -m '${commitMsg}'`);
+    gitPush(commitMsg);
   });
 }
 
 if (!gitIsClean()) {
-  console.log('注意：当前工作区不干净，已自动保存当前更改');
+  const commitMsg = '注意：当前工作区不干净，已自动保存当前更改';
+  console.log(commitMsg);
   execSync(`git add .`);
-  execSync(`git commit -m '注意：当前工作区不干净，已自动保存当前更改'`);
+  execSync(`git commit -m ${commitMsg}`);
+  gitPush(commitMsg);
   main();
 } else {
   main();
